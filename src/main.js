cd1c8e72be75d5783d55a1b3f950080dd2052c14
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-01 19:49:15 +0530

    method: "POST"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // fetch
 // 2nd parameter which is usually or always I should say is object***
 // "GET";
-
+/*
 const getDadJoke = async () => {
     const response = await fetch("https://icanhazdadjoke.com", {
         method: "GET",
@@ -14,10 +14,27 @@ const getDadJoke = async () => {
     console.log(jsonJokeData);
 }
 
-getDadJoke();
+getDadJoke();  */
 
+// "POST"
+const jokeObject = {
+    id: 'OfNRfaaUSf', 
+    joke: 'When I left school, I passed every one of my exams… Greek Mythology. It always was my achilles elbow'
+}
+// this is going to a different test API "https:httpbin.org/post"
+const postDadJoke = async (jokeObj) => {
+    const response = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(jokeObj)
+    })
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+}
 
-
+postDadJoke(jokeObject);
 
 
 
