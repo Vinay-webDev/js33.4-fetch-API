f0ab17d7b4b1a1e81b690d5252766153cbdec28d
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-01 20:10:24 +0530

    one more time!!!!!!!!!!

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ const getDadJoke = async () => {
 getDadJoke();  */
 
 // "POST"
+/*
 const jokeObject = {
     id: 'OfNRfaaUSf', 
     joke: 'When I left school, I passed every one of my exams… Greek Mythology. It always was my achilles elbow'
@@ -34,12 +35,28 @@ const postDadJoke = async (jokeObj) => {
     console.log(jsonResponse);
 }
 
+postDadJoke(jokeObject); */
+
+// one more time for practice***
+const jokeObject = {
+    id: 'OfNRfaaUSf', 
+    joke: 'When I left school, I passed every one of my exams… Greek Mythology. It always was my achilles elbow'
+}
+const postDadJoke = async (jokeObj) => {
+    const response = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jokeObj),
+    })
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+}
+
 postDadJoke(jokeObject);
 
-
-
-
-
+// url
 
 
 
